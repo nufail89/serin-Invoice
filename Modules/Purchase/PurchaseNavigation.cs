3b578fce748dcf7b37e43848662a3c1b1fb1bc86
Muhammad Nufail <nuvailmizzan@gmail.com>
2022-06-13 07:49:42 +0700

    main

--- Modules/Purchase/PurchaseNavigation.cs
+++ Modules/Purchase/PurchaseNavigation.cs
@@ -3,7 +3,7 @@ using MyPages = Indotalent.Purchase.Pages;
 
 
 [assembly: NavigationMenu(6900, "Purchase", icon: "fa-shopping-bag")]
-[assembly: NavigationLink(6900, "Purchase/Vendor", typeof(MyPages.VendorController), icon: "fa-folder")]
-[assembly: NavigationLink(6900, "Purchase/Purchase Order", typeof(MyPages.PurchaseOrderController), "fa-folder")]
-[assembly: NavigationLink(6900, "Purchase/Bill", typeof(MyPages.BillController), icon: "fa-folder")]
-[assembly: NavigationLink(6900, "Purchase/Bill Payment", typeof(MyPages.BillPaymentController), "fa-folder")]
+[assembly: NavigationLink(6900, "Purchase/Vendor", typeof(MyPages.VendorController), icon: "fa-list")]
+[assembly: NavigationLink(6900, "Purchase/Purchase Order", typeof(MyPages.PurchaseOrderController), "fa-list")]
+[assembly: NavigationLink(6900, "Purchase/Bill", typeof(MyPages.BillController), icon: "fa-list")]
+[assembly: NavigationLink(6900, "Purchase/Bill Payment", typeof(MyPages.BillPaymentController), "fa-list")]
--- Modules/Sales/SalesNavigation.cs
+++ Modules/Sales/SalesNavigation.cs
@@ -3,8 +3,8 @@ using MyPages = Indotalent.Sales.Pages;
 
 
 [assembly: NavigationMenu(5900, "Sales", icon: "fa-shopping-cart")]
-[assembly: NavigationLink(5900, "Sales/Sales Channel", typeof(MyPages.SalesChannelController), icon: "fa-folder")]
-[assembly: NavigationLink(5900, "Sales/Customer", typeof(MyPages.CustomerController), icon: "fa-folder")]
-[assembly: NavigationLink(5900, "Sales/Sales Order", typeof(MyPages.SalesOrderController), icon: "fa-folder")]
-[assembly: NavigationLink(5900, "Sales/Invoice", typeof(MyPages.InvoiceController), icon: "fa-folder")]
-[assembly: NavigationLink(5900, "Sales/Invoice Payment", typeof(MyPages.InvoicePaymentController), icon: "fa-folder")]
+[assembly: NavigationLink(5900, "Sales/Sales Channel", typeof(MyPages.SalesChannelController), icon: "fa-list")]
+[assembly: NavigationLink(5900, "Sales/Customer", typeof(MyPages.CustomerController), icon: "fa-list")]
+[assembly: NavigationLink(5900, "Sales/Sales Order", typeof(MyPages.SalesOrderController), icon: "fa-list")]
+[assembly: NavigationLink(5900, "Sales/Invoice", typeof(MyPages.InvoiceController), icon: "fa-list")]
+[assembly: NavigationLink(5900, "Sales/Invoice Payment", typeof(MyPages.InvoicePaymentController), icon: "fa-list")]
